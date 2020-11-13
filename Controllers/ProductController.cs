c8c7a26d7ae12cee059c9593a6561daf4ddb3f08
Henrico <henrico.almeida@fcamara.com.br>
2020-11-13 20:45:19 -0300

    feat: add ProductController

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ProductCatalog.Models;
+using ProductCatalog.Repositories;
+using ProductCatalog.ViewModels.ProductViewModels;
+
+namespace ProductCatalog.Controllers
+{
+    [Route("v1/products")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly ProductRepository _repository;
+
+        public ProductController(ProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        
+        [HttpGet]
+        public IEnumerable<ListProductViewModel> Get()
+        {
+            return _repository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return _repository.Get(id);
+        }
+
+        [HttpPost]
+        public ResultViewModel Post([FromBody]EditorProductViewModel model)
+        {
+            model.Validate();
+            if (model.Invalid)
+                return new ResultViewModel
+                {
+                    Success = false,
+                    Message = "Não foi possível cadastrar o produto",
+                    Data = model.Notifications
+                };
+
+            var product = new Product();
+            product.Title = model.Title;
+            product.CategoryId = model.CategoryId;
+            product.CreateDate = DateTime.Now; // Nunca recebe esta informação
+            product.Description = model.Description;
+            product.Image = model.Image;
+            product.LastUpdateDate = DateTime.Now; // Nunca recebe esta informação
+            product.Price = model.Price;
+            product.Quantity = model.Quantity;
+
+            _repository.Save(product);
+
+            return new ResultViewModel
+            {
+                Success = true,
+                Message = "Produto cadastrado com sucesso!",
+                Data = product
+            };
+        }
+
+        [HttpPost]
+        public ResultViewModel Post([FromBody]Product product)
+        {
+            _repository.Save(product);
+
+            return new ResultViewModel
+            {
+                Success = true,
+                Message = "Produto cadastrado com sucesso!",
+                Data = product
+            };
+        }
+
+        [HttpPut]
+        public ResultViewModel Put([FromBody]EditorProductViewModel model)
+        {
+            model.Validate();
+            if (model.Invalid)
+                return new ResultViewModel
+                {
+                    Success = false,
+                    Message = "Não foi possível alterar o produto",
+                    Data = model.Notifications
+                };
+
+            var product = _repository.Get(model.Id);
+            product.Title = model.Title;
+            product.CategoryId = model.CategoryId;
+            // product.CreateDate = DateTime.Now; // Nunca altera a data de criação
+            product.Description = model.Description;
+            product.Image = model.Image;
+            product.LastUpdateDate = DateTime.Now; // Nunca recebe esta informação
+            product.Price = model.Price;
+            product.Quantity = model.Quantity;
+
+            _repository.Update(product);
+
+            return new ResultViewModel
+            {
+                Success = true,
+                Message = "Produto alterado com sucesso!",
+                Data = product
+            };
+        }
+    }
+}
